d31b3ad85534757c04d5ddc1eb1bf49bb02a4d7c
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-05-03 19:03:30 +0530

    create Base class for creating db table

--- models.py
+++ models.py
@@ -3,3 +3,5 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context
+
+Base = declarative_base()
